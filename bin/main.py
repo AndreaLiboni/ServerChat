cc5ab6225d56ff7a490d5f569049bf4e85d17dd3
utente01 <francescomartino498@gmail.com>
2019-11-05 11:48:09 +0100

    private message

--- bin/main.py
+++ bin/main.py
@@ -9,7 +9,6 @@ users = []
 
 #   FUNZIONI
 def connect(sockCli, host):
-    username = ""
     while (True):
         sock.listen(4)
         pack = sockCli.recv(1024)
@@ -26,9 +25,12 @@ def connect(sockCli, host):
                     sockCli.send(error(err))
                 else:
                     sockCli.send(OK())
-                    username = err
-            elif pack[0] == 12:
-                err = logout(username)
+            elif pack[0] == 22:
+                err = privateMessage(pack, host)
+                if err:
+                    sockCli.send(error("Messaggio non inviato"))
+                else:
+                    sockCli.send(error(OK()))
 
 
 def OK():
@@ -36,6 +38,7 @@ def OK():
     mex.append(0)
     mex += (0).to_bytes(2, byteorder="big")
     return mex
+
 
 def error(string):
     mex = bytearray()
@@ -45,11 +48,6 @@ def error(string):
     mex += data
     return mex
 
-def logout(username):
-    for i in range (len(users)):
-        user = users[i]
-        if user[0] == username:
-            del users[i]
 
 def login(pack, sockCli):
     i = 3
@@ -76,9 +74,10 @@ def login(pack, sockCli):
                 return "Passoword non trovata"
     return "Username non trovato"
 
+
 def registrazione(pack):
     i = 3
-    cc = True   #ContaCampi
+    cc = True  # ContaCampi
     username = ""
     password = ""
     while (i < len(pack)):
@@ -97,13 +96,61 @@ def addUser(username, password):
     fopen = open("../User/users.csv", "r")
     for line in fopen:
         campi = line.split(";")
-        if campi[0].replace("\"","") == username:
+        if campi[0].replace("\"", "") == username:
             fopen.close()
             return True
     fopen = open("../User/users.csv", "a")
-    fopen.write("\"" + username +"\";\"" + password + "\"\n")
+    fopen.write("\"" + username + "\";\ "" + password + ""\" \n")
     fopen.close()
     return False
+
+
+def privateMessage(pack, host):
+    i = 3
+    cc = True  # ContaCampi
+    dest = ""
+    text = ""
+    while (i < len(pack)):
+        if pack[i] == 0:
+            cc = False
+            i += 1
+        if cc:
+            dest += chr(pack[i])
+        else:
+            text += chr(pack[i])
+        i += 1
+
+    # da finire dopo aver finito l'array users
+    # ciclo sul nome per trovare il socket
+    # uffa
+    # pack_to_send = createMexPack(23, mitt, text)
+    # sock.send(pack_to_send, socket trovato)
+
+    # fare controllo con return True/False
+
+
+def createMexPack(mode, mitt, text):
+    mex = bytearray()
+
+    mex.append(mode)
+    info = dataToBytes([mitt, text])
+    mex.append(len(info).to_bytes(2, byteorder="big"))
+    mex.append(info)
+
+    if boolD:
+        print(mex)
+
+    return mex
+
+
+def dataToBytes(data):
+    bytes = bytearray()
+    for d in data:
+        bytes += (bytearray(d.encode()))
+        if data.index != len(data) - 1:
+            bytes.append(0)
+    return bytes
+
 
 #   MAIN
 if __name__ == "__main__":
