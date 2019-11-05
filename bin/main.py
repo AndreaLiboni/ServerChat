fc3a327fa9dbc1002fc5140c4f9356f4f8c1c13e
utente01 <francescomartino498@gmail.com>
2019-11-05 11:31:17 +0100

    private message

--- bin/main.py
+++ bin/main.py
@@ -135,7 +135,8 @@ def createMexPack(mode, mitt, text):
     mex.append(len(info).to_bytes(2, byteorder="big"))
     mex.append(info)
 
-    print(mex)
+    if boolD:
+        print(mex)
 
     return mex
 
